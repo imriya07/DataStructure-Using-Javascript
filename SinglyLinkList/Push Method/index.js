cd80c0c03f6f83869c657fe4413357f989acd9ae
imriya07 <riya01022001@gmail.com>
2022-04-30 13:28:10 +0530

    update index

---
+++ SinglyLinkList/Push Method/index.js
@@ -0,0 +1,26 @@
+class Node{
+    constructor(val){
+        this.data = val;
+        this.next = null;
+    }
+}
+class SinglyLinkList{
+    constructor(){
+        this.head = null;
+        this.next = null;
+        this.length = 0;
+    }
+    push(x) {
+        let newNode = new Node(x);
+        if(this.head == null){
+            this.head = newNode
+            this.tail = newNode
+        } else{
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length = this.length + 1
+        return this.length
+    }
+}
+const ar = new SinglyLinkList();
