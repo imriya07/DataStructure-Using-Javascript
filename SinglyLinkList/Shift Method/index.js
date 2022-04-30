f9e2ee3769d88f277413d170ad733bfac3a6f73e
imriya07 <riya01022001@gmail.com>
2022-04-30 15:10:00 +0530

    update index

---
+++ SinglyLinkList/Shift Method/index.js
@@ -0,0 +1,61 @@
+class Node{
+    constructor(val){
+        this.data = val;
+        this.next = null;
+    }
+}
+class SinglyLinkList{
+    constructor(){
+        this.head = null;
+        this.next = null;
+        this.length = 0;
+    }
+    push(x) {
+        let newNode = new Node(x);
+        if(this.head == null){
+            this.head = newNode
+            this.tail = newNode
+        } else{
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length = this.length + 1
+        return this.length
+    }
+    pop() {
+        if(this.head == null){
+            return "No node"
+        }
+        let a = this.head;
+        let b = a;
+        while(b.next){
+            a = b
+            b = b.next
+        }
+        this.tail = a;
+        this.tail.next = null;
+        this.length = this.length - 1;
+        if(this.length == 0){
+            this.tail = null
+            this.head = null
+        }
+        return b
+    }
+    shift(){
+        if(this.head == null){
+            return "No node"
+        }
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        currentHead.next = null;
+        this.length = this.length - 1;
+        if(this.length == 0){
+        this.tail = null
+        }
+        return currentHead
+    }
+    
+}
+const ar = new SinglyLinkList();
+
+
