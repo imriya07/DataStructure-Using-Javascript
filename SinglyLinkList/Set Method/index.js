1b440c6cef0aec62c0518bbb1d7f12ff70d8da6e
imriya07 <riya01022001@gmail.com>
2022-05-03 11:19:56 +0530

    update index

---
+++ SinglyLinkList/Set Method/index.js
@@ -0,0 +1,9 @@
+set(index, val){
+    var foundNode = this.get(index);
+    if(foundNode){
+        foundNode,val = val;
+        return true;
+    }
+    return false;
+}
+var list = new SinglyLinkedList();
