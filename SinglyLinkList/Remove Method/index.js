58c44597f88e53e12dfe61c5c93166aacc2bb5ca
imriya07 <riya01022001@gmail.com>
2022-05-03 13:36:13 +0530

    update index

---
+++ SinglyLinkList/Remove Method/index.js
@@ -0,0 +1,11 @@
+remove(index){
+    if(index < 0 || index >= this.length) return undefined;
+    if(index === 0) return !!this.shift();
+    if(index === this.length - 1) return this.Pop();
+    var previousNode = this.get(index - 1);
+    var removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+}
+var list = new SinglyLinkedList();
