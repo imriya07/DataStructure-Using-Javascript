c50b181e1259893be2fffd3afba078fe7ffddfc0
imriya07 <riya01022001@gmail.com>
2022-05-04 13:26:26 +0530

    update index

---
+++ Stacks/Array implementation/index.js
@@ -0,0 +1,36 @@
+//push, pop -----------------------------------------------------
+
+var stack = []
+console.log(stack);
+
+stack.push("Linkdin");
+console.log(stack);
+
+stack.push("Amazon");
+console.log(stack);
+
+stack.push("Google");
+console.log(stack);
+
+stack.pop("")
+console.log(stack);
+
+//shift, unshift --------------------------------------
+
+var stack = []
+console.log(stack);
+
+stack.unshift("John");
+console.log(stack);
+
+stack.unshift("Hey");
+console.log(stack);
+
+stack.unshift("Tilda");
+console.log(stack);
+
+stack.shift("");
+console.log(stack);
+
+stack.shift("");
+console.log(stack);
