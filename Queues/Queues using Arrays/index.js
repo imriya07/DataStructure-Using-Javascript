d263d6f26d089eefb072963206bd5c941bb616ef
imriya07 <riya01022001@gmail.com>
2022-05-04 14:18:35 +0530

    update index

---
+++ Queues/Queues using Arrays/index.js
@@ -0,0 +1,26 @@
+var q = [];
+console.log(q);
+
+q.push("First");
+console.log(q);
+
+q.push("Second");
+console.log(q);
+
+q.push("Third");
+console.log(q);
+
+q.shift("First");
+console.log(q);q
+
+q.unshift("First");
+console.log(q);q
+
+q.unshift("Second");
+console.log(q);q
+
+q.pop("First");
+console.log(q);
+
+q.push("Second");
+console.log(q);
