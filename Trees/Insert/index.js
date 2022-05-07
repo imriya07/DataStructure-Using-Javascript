b02c886bc93ef43c77545a116e861974b655f813
imriya07 <riya01022001@gmail.com>
2022-05-07 20:39:46 +0530

    update index

---
+++ Trees/Insert/index.js
@@ -0,0 +1,38 @@
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        var newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        } else{
+            var current = this.root;
+            while(true){
+                if(value < current.value){
+                    if(current.left === null){
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (value > current.value){
+                    if(current.right === null){
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+}
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
